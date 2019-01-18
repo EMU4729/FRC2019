617c11e87130eeb2ef8fdcff8398b85d7949cd87
LAPTOP <32506926+unswfrc@users.noreply.github.com>
2019-01-18 18:35:43 +1100

    align done but not tested check endVisible

--- src/main/java/org/usfirst/frc4729/FRC2019/commands/Align.java
+++ src/main/java/org/usfirst/frc4729/FRC2019/commands/Align.java
@@ -1,11 +1,15 @@
 package org.usfirst.frc4729.FRC2019.commands;
 
+import org.usfirst.frc4729.FRC2019.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Align extends Command {
   public Align() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.vision);
+    requires(Robot.drive);
   }
 
   // Called just before this Command runs the first time
@@ -16,6 +20,38 @@ public class Align extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double forwards = 0;
+    double sideways = 0;
+    double turn = 0;
+
+    //is the line in the center
+    if (Robot.vision.offset.x > 10){
+      //go right
+      sideways = 1;
+    } else if (Robot.vision.offset.y < -10) {
+      //go left
+      sideways = -1;
+    }
+
+    //can you see end of line
+    if (Robot.vision.endVisible == true) {
+      //go foward
+      forwards = 1;
+    } else if (Robot.vision.endVisible == false) {
+      //stop
+      forwards = 0;
+    }
+
+    //is the line straight
+    if (Robot.vision.angle > 5) {
+      //rotate right
+      turn = 1;
+    } else if (Robot.vision.angle < -5) {
+      //rotate left
+      turn = -1;
+    }
+
+    Robot.drive.omni(forwards, sideways, turn);
   }
 
   // Make this return true when this Command no longer needs to run execute()
